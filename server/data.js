2cacc6054f12a693279e1ecfa4ed23a7db6f8e6c
e4rache <e4rache@protonmail.com>
2018-09-02 13:20:16 +0200

    .oOo.

--- server/data.js
+++ server/data.js
@@ -1,54 +1,54 @@
 const authors = [
     {
         id: 1,
-        name: 'George Smith'
+        name: 'Suzanne Collins'
     },
     {
         id: 2,
-        name: 'Anthony Parker'
+        name: 'Christina Dalcher'
     },
     {
         id: 3,
-        name: 'Andrew Mathews'
+        name: 'Jane Austen'
     }
 ]
 
 const books = [
     {
         id: 1,
-        title: 'The Complete Node.js Developer Course',
-        genre: 'node.js',
+        title: 'The Hunger Games',
+        genre: 'Science Fiction',
         authorId: 1,
     },
     {
         id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        genre: 'node.js',
-        authorId: 2,
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        genre: 'javascript',
+        title: 'Catching Fire',
+        genre: 'Science Fiction',
         authorId: 1,
     },
     {
-        id: 4,
-        title: 'What is Java Spring boot all about ?',
-        genre: 'java',
-        authorId: 3
+        id: 3,
+        title: 'Vox',
+        genre: 'Science Fiction',
+        authorId: 2,
     },
     {
-        id: 5,
-        title: 'A JavaScript / GraphQL story',
-        genre: 'graphql',
+        id: 4,
+        title: 'Ellipsis Zine One: A flash fiction anthology',
+        genre: 'Short Stories',
         authorId: 2
     },
     {
+        id: 5,
+        title: 'Pride and Prejudice',
+        genre: 'Historical',
+        authorId: 3
+    },
+    {
         id: 6,
-        title: 'front-end development with React tutorial',
-        genre: 'react',
-        authorId: 1
+        title: 'Sense and Sensibility',
+        genre: 'Classics',
+        authorId: 3
     }
 ]
 
